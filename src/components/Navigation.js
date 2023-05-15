4f4c5f4baaadbaeeceb5363cc09019894d0eaf52
Huynh Van Phuot <phuothuynh2002@gmail.com>
2023-05-15 22:12:54 +0700

    Functional Components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,25 @@
-import React, { Component } from "react";
-export default class Navigation extends Component {
-    render() {
-        return (
-            <div>
-                <nav>
-                    <ul className="heading">
-                        <li className="heading-title"><a className="active" href="#home">Home</a></li>
-                        <li className="heading-title"><a href="#news">News</a></li>
-                        <li className="heading-title"><a href="#about">About</a></li>
-                        <li className="heading-title"><a href="#contact">Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
-        )
-    }
+import React from "react";
+export default function Navigation() {
+  return (
+    <div>
+      <nav>
+        <ul className="heading">
+          <li className="heading-title">
+            <a className="active" href="#home">
+              Home
+            </a>
+          </li>
+          <li className="heading-title">
+            <a href="#news">News</a>
+          </li>
+          <li className="heading-title">
+            <a href="#about">About</a>
+          </li>
+          <li className="heading-title">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
